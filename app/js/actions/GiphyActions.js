dc46ab3d89442664ff73503a0cf8948ff783383f
Arinde Eniola <eniola.arinde@andela.co>
2015-09-27 12:42:08 +0100

    fix bug and also complete the actionCreator for loading trending giphys

---
+++ app/js/actions/GiphyActions.js
@@ -0,0 +1,35 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var giphy = require('giphy-api')();
+
+//Note: Find a better way of handling this issue.
+//This state holder is supposed to be cleared after a period
+var loadedGiphys = [];
+
+var GiphyActions = {
+
+  loadTrendingGiphys: function() {
+
+    var action = function(payload) {
+      return {
+        actionType: "LOAD_TRENDING_GIPHYS",
+        data: payload
+      }
+    }
+
+    if(loadedGiphys.length === 0) {
+      giphy.trending({
+        limit: 1,
+        rating: 'g',
+        fmt: 'json'
+      },function(err, res) {
+        loadedGiphys = res.data;
+        AppDispatcher.dispatch(action(res.data));
+      });
+    }
+    else {
+      AppDispatcher.dispatch(action(loadedGiphys));
+    }
+  }
+}
+
+module.exports = GiphyActions;
--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 var React = require('react');
 
 var GiphyView = require('./components/GiphyView');
-var SearchBar = require('./components/SearchBar')
+var SearchBar = require('./components/SearchBar');
+var GiphyActions = require('./actions/GiphyActions');
 
 var ipc = window.require('ipc');
 require('../css/style.css');
@@ -11,32 +12,37 @@ var Giphy = React.createClass({
   render: function() {
 
     return (
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header className="mdl-layout__header header-style">
-          <div className="mdl-layout__header-row header-content-style">
-            <span className="mdl-layout-title">
-              <img src="assets/giphy.png" className="header-image"/>
-            </span>
-          </div>
-        </header>
-        <main className="mdl-layout__content">
-          <div className="page-content">
-            <div className="mdl-grid">
-              <div className="mdl-cell mdl-cell--12-col giphy-view">
-                <GiphyView />
-              </div>
-              <div className="mdl-cell mdl-cell--12-col search-bar">
-                <SearchBar />
+      <div className="mdl-layout__container">
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <header className="mdl-layout__header header-style">
+            <div className="mdl-layout__header-row header-content-style">
+              <span className="mdl-layout-title">
+                <img src="assets/giphy.png" className="header-image"/>
+              </span>
+            </div>
+          </header>
+          <main className="mdl-layout__content">
+            <div className="page-content">
+              <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--12-col giphy-view">
+                  <GiphyView />
+                </div>
+                <div className="mdl-cell mdl-cell--12-col search-bar">
+                  <SearchBar />
+                </div>
               </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
     );
   }
 })
 
 ipc.on('message', function(message) {
+  if(message === 'load-giphys') {
+    GiphyActions.loadTrendingGiphys();
+  }
 });
 
 React.render(<Giphy />, document.getElementById('giphy'));
